e95307c6968345d4a19011dbceb804c0daec8958
fumiya-oobayashi-pixel <fumiya-oobayashi@aichi-hino.co.jp>
2026-07-13 09:40:51 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -15,6 +15,7 @@ const MASTER_ADMIN_EMAILS = [
   "michio-sakae@aichi-hino.co.jp",
   "fumiya-oobayashi@aichi-hino.co.jp",
   "T_SOS@aichi-hino.co.jp",
+  "junya-yoshida@aichi-hino.co.jp",
   // 追加する場合はここに追記
   // "xxx@aichi-hino.co.jp",
 ];
